4fd7d154c534c2b012f967885d21ac4c3f507cfa
halilbeytorun <beytorun25@gmail.com>
2024-05-26 16:14:25 +0300

    add sqlite3 into project
    before continuing with sqlite3 or db implementation, should adjust the
    folders therefore the code would get out from the UIModule folder

--- UIModule/DataManagementModule/DataManagementModule.cpp
+++ UIModule/DataManagementModule/DataManagementModule.cpp
@@ -1,3 +1,27 @@
 #include "DataManagementModule.h"
 
 
+#include <sqlite3.h>
+
+
+int DataManagementModule::InitializeDB(const std::string& db_loc)
+{
+    int return_code{};
+    return_code = sqlite3_open(db_loc.c_str(), &m_db);
+    if(SQLITE_OK == return_code)
+    {
+        return return_code;
+    }
+    else
+    {
+        sqlite3_errmsg16(m_db);
+    }
+}
+
+DataManagementModule::~DataManagementModule()
+{
+    if(m_db)
+    {
+        sqlite3_close(m_db);
+    }
+}
--- UIModule/DataManagementModule/DataManagementModule.h
+++ UIModule/DataManagementModule/DataManagementModule.h
@@ -1,10 +1,20 @@
 #ifndef DataManagementModule_H
 #define DataManagementModule_H
 
+#include <string>
+
+
+struct sqlite3;
 
 /// @brief Deals with the storage and retrieval of financial data, user data. Uses SQLite database to store related data.
 class DataManagementModule
 {
+public:
+    int InitializeDB(const std::string&);
+    ~DataManagementModule();
+
+private:
+    sqlite3* m_db{};
 
 };
 
